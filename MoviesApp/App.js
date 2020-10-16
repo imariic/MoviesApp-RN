40f4d3ef06cd6c725a73280f4a6b1ee5b2c1d37c
Ivan Mariić <imariic77@gmail.com>
2020-10-16 21:48:35 +0200

    StatusBar implementation

--- MoviesApp/App.js
+++ MoviesApp/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AppStatusBar from "./components/AppStatusBar/AppStatusBar";
 import Home from "./components/Home/Home";
 
 const Stack = createStackNavigator();
@@ -10,6 +11,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
+      <AppStatusBar />
       <Stack.Navigator screenOptions={{ headerTitleAlign: 'center', headerTintColor: "cyan" }}>
         <Stack.Screen options={{
           headerTitle: (props) => (<Image style={{ width: 200, height: 50 }} source={require('./assets/headertitle.png')} resizeMode="contain" />), headerStyle: {
---
+++ MoviesApp/components/AppStatusBar/AppStatusBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, StatusBar, View } from "react-native";
+
+const AppStatusBar = () => {
+    return (
+        <View>
+            <StatusBar backgroundColor="#0B253F" />
+        </View>
+    );
+}
+
+
+export default AppStatusBar;
